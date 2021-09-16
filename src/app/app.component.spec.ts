4b5e240fdb84ed6f6d20256335f6fe0f524ad3f0
Mirum-cor <laetitia-caritas@mail.ru>
2021-09-17 01:46:57 +0300

    feat: add footer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent,
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 

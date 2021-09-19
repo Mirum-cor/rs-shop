81fbb1b673b81973790a494f2016641c6ffccd2d
Mirum-cor <laetitia-caritas@mail.ru>
2021-09-19 22:36:38 +0300

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProductCategoryComponent } from './components/product-category/product-category.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'category/:id', component: ProductCategoryComponent },
+  { path: 'product/:id', component: ProductComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProductCategoryComponent } from './components/product-category/product-category.component';
+import { ProductComponent } from './components/product/product.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    MainPageComponent,
+    ProductCategoryComponent,
+    ProductComponent,
     FooterComponent,
   ],
   imports: [

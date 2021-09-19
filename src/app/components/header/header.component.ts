7492e95bed6e8759910b7d7b2a0324b371b8076b
Mirum-cor <laetitia-caritas@mail.ru>
2021-09-20 01:23:07 +0300

    feat: add geolocation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,6 +12,8 @@ import { RSSState } from 'src/app/store/rss.state';
 })
 export class HeaderComponent implements OnInit {
   @ViewChild("restOfContacts") restOfContacts: ElementRef = { nativeElement: '' };
+  @ViewChild("currentCity") currentCity: ElementRef = { nativeElement: '' };
+  @ViewChild("biggestCities") biggestCities: ElementRef = { nativeElement: '' };
 
   @Select(RSSState.categories) public categories$!: Observable<ICategory[]>;
 
@@ -22,15 +24,56 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.store.dispatch(new GetCategories([]));
     this.categories = this.store.selectSnapshot(RSSState.categories);
+    this.getCurrentLocation();
+  }
+
+  getCurrentLocation(): void {
+    let currentPos: string[] = [];
+    const geoSuccess = (position: any) => {
+      const startPos = position;
+      currentPos = [
+        startPos.coords.latitude.toFixed(3),
+        startPos.coords.longitude.toFixed(3),
+      ];
+      this.getCurrentCity(currentPos);
+    };
+    navigator.geolocation.getCurrentPosition(geoSuccess);
+  }
+
+  getCurrentCity([lat, lng]: string[]): void {
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=81aaf0c0775f440885be1a9e23fdc1ea`)
+      .then((responce) => {
+        if (!responce.ok) throw new Error(`Problem with geocoding ${responce.status}!`);
+        return responce.json();
+      })
+      .then((data) => {
+        this.currentCity.nativeElement.textContent = data.results[0].components.city;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  closeDropdowns(): void {
+    if (!this.restOfContacts.nativeElement.classList.contains('invisible')) {
+      this.restOfContacts.nativeElement.classList.add('invisible');
+    } else if (!this.biggestCities.nativeElement.classList.contains('invisible')) {
+      this.biggestCities.nativeElement.classList.add('invisible');
+    }
+  }
+
+  chooseCity(event: Event): void {
+    const target = event.target as HTMLElement;
+    if (target.nodeName === 'P') {
+      this.currentCity.nativeElement.textContent = target.textContent;
+    }
+  }
+
+  toggleBiggestCities(event: Event): void {
+    this.biggestCities.nativeElement.classList.toggle('invisible');
+    event.stopPropagation();
   }
 
   toggleRestOfContacts(event: Event): void {
-    const target = event.target as HTMLElement;
-    if (target.textContent === 'Ещё') {
-      this.restOfContacts.nativeElement.classList.toggle('invisible');
-    } else if (!this.restOfContacts.nativeElement.classList.contains('invisible')) {
-      this.restOfContacts.nativeElement.classList.add('invisible');
-    }
+    this.restOfContacts.nativeElement.classList.toggle('invisible');
     event.stopPropagation();
   }
 }

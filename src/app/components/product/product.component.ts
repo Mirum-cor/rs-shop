c27d79dfabdcb20071251813b995eb893e48cf64
Mirum-cor <laetitia-caritas@mail.ru>
2021-09-26 01:44:38 +0300

    feat: add description and characteristics of product (including popularity icon) to product page

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,6 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 import { ICategory } from 'src/app/services/category.interface';
 import { IProduct } from 'src/app/services/product.interface';
 import { ISubcategory } from 'src/app/services/subcategory.interface';
@@ -13,7 +12,7 @@ import { RSSState } from 'src/app/store/rss.state';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.less'],
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, AfterViewChecked {
   @Select(RSSState.currentProduct) public currentProduct$!: Observable<string>;
 
   @Input() product: IProduct = {
@@ -31,6 +30,10 @@ export class ProductComponent implements OnInit {
   @ViewChild('subCategory') subCategory: ElementRef = { nativeElement: '' };
 
   @ViewChild('mainImage') mainImage: ElementRef = { nativeElement: '' };
+
+  @ViewChild('rating') rating: ElementRef = { nativeElement: '' };
+
+  @ViewChild('inStock') inStock: ElementRef = { nativeElement: '' };
 
   constructor(private store: Store) {}
 
@@ -65,6 +68,49 @@ export class ProductComponent implements OnInit {
         this.product = { ...this.product, isInCart: true };
       }
     });
+  }
+
+  ngAfterViewChecked(): void {
+    this.setCorrespondingRating();
+    this.setCorrespondingInStockStyle();
+  }
+
+  setCorrespondingRating(): void {
+    console.log(this.rating.nativeElement.children.length, this.product.rating);
+    if (this.rating.nativeElement.children.length !== this.product.rating) {
+      this.rating.nativeElement.innerHTML = '';
+      for (let i = 1; i <= this.product.rating; i++) {
+        const img = document.createElement('img');
+        img.src = '../../../assets/rate-star.svg';
+        img.alt = 'rate-star';
+        this.rating.nativeElement.append(img);
+      }
+    }
+  }
+
+  setCorrespondingInStockStyle(): void {
+    if (this.product.availableAmount >= 20) {
+      this.inStock.nativeElement.style.color = '#008f30';
+      this.inStock.nativeElement.style.background =
+        'url(../../../assets/much-products.svg) no-repeat left -1px';
+    } else if (this.product.availableAmount < 5) {
+      this.inStock.nativeElement.style.color = '#b10000';
+      this.inStock.nativeElement.style.background =
+        'url(../../../assets/few-products.svg) no-repeat left -1px';
+    } else if (
+      this.product.availableAmount >= 5 &&
+      this.product.availableAmount < 20
+    ) {
+      this.inStock.nativeElement.style.color = '#d3c500';
+      this.inStock.nativeElement.style.background =
+        'url(../../../assets/enough-products.svg) no-repeat left -1px';
+    } else {
+      this.inStock.nativeElement.style.color = '#b10000';
+      this.inStock.nativeElement.textContent = 'Нет в наличии';
+    }
+    this.inStock.nativeElement.style.paddingLeft = '25px';
+    this.inStock.nativeElement.style.backgroundSize = '20px';
+    this.inStock.nativeElement.style.textShadow = '0 0 1px #000000';
   }
 
   showImageBigger(event: Event): void {
